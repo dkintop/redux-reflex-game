0c3c14678fbd29aac5eda2605c6658dccaaa9043
Doug Kintop <dkintop@hotmail.com>
2020-02-11 12:05:23 -0500

    implements shuffle algorithm to randomize position in grid of active game-tile

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,21 +1,26 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { resetGame } from "../actions/index.js";
+import { shuffle } from "../algorithms/shuffle.js";
 export class Game extends Component {
+  matrix = [
+    <div className="game-tile-active">d</div>,
+    <div className="game-tile"></div>,
+    <div className="game-tile">d</div>,
+    <div className="game-tile">d</div>,
+    <div className="game-tile">d</div>,
+    <div className="game-tile">d</div>,
+    <div className="game-tile">d</div>,
+    <div className="game-tile">d</div>,
+    <div className="game-tile">d</div>
+  ];
+
+  shuffledMatrix = () => {
+    return shuffle(this.matrix);
+  };
+
   render() {
-    return (
-      <div id="game-board">
-        <div className="game-tile">d</div>
-        <div className="game-tile"></div>
-        <div className="game-tile">d</div>
-        <div className="game-tile">d</div>
-        <div className="game-tile">d</div>
-        <div className="game-tile">d</div>
-        <div className="game-tile">d</div>
-        <div className="game-tile">d</div>
-        <div className="game-tile">d</div>
-      </div>
-    );
+    return <div id="game-board">{this.shuffledMatrix()}</div>;
   }
 
   componentWillUnmount() {
